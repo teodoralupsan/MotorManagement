75baaa7faad245f37b438133e35cd5483d60c9a4
Teodora Lupsan <teodora_pintea_ramona@yahoo.com>
2020-04-29 16:16:41 +0300

    Creating Database using EF and Identity Model

---
+++ MotorManagement.API/DataAccess/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess
+{
+    public class DataContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>,
+        IdentityRoleClaim<int>, IdentityUserToken<int>>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+                userRole.HasOne(u => u.User)
+                    .WithMany(u => u.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+        }
+    }
+}
---
+++ MotorManagement.API/Domain/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public class User : IdentityUser<int>
+    {
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
